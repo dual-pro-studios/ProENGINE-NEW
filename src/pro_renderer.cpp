b9832ba41707e0c9f2ef8797b6eb06f749e53f9c
Altan Haan <altan.h@gmail.com>
2013-04-29 17:34:22 +0800

    Added comments to pro_renderer.
    Added comments to pro_renderer.cpp and pro_renderer.hpp.

--- src/pro_renderer.cpp
+++ src/pro_renderer.cpp
@@ -3,61 +3,77 @@
 using namespace pro;
 using namespace std;
 
+// Default Initializer
 renderer::renderer(void) {
 
 }
 
-
+// Destructor
 renderer::~renderer(void) {
 
 }
 
+// Starts the renderer with the given params
+// bool fullscreen(start in fullscreen mode or not) NOTE: if you start in fullscreen, the width and height params will be ignored.
+// float width(start with the given width)
+// float height(start with the given height)
+// const string title(the title of the window)
 void renderer::start(bool fullscreen, float width, float height, const string title) {
+    // If fullscreen, start with the desktop resolution
 	if(fullscreen) {
 		window.create(sf::VideoMode::getDesktopMode(), title, sf::Style::Fullscreen);
 		windowCam.setSize((float)sf::VideoMode::getDesktopMode().width, (float)sf::VideoMode::getDesktopMode().height);
 	}
+    // Otherwise start with the given width and height
 	else {
 		window.create(sf::VideoMode((int)width, (int)height), title);
-		windowCam.setSize(width, height);
+		windowCam.setSize(width, height); // Set the camera size
 	}
+    // Make the window viewport the camera's
 	window.setView(windowCam.getCameraView());
 }
 
+// Begin drawing the things
 void renderer::begin_scene() {
-	window.setView(windowCam.getCameraView());
-	window.clear(sf::Color::Color(0, 0, 0, 0));
+	window.setView(windowCam.getCameraView()); // Update the window's view
+	window.clear(sf::Color::Color(0, 0, 0, 0)); // Clear the screen with black
 }
 
+// Stop drawing the things and display them
 void renderer::end_scene() {
-	window.display();
-	fpsTime = fpsClock.restart();
-	fps[0] = fpsTime.asSeconds();
-	fps[1] = 1.f / fpsTime.asSeconds();
+	window.display(); // Display whatever has been drawn
+	fpsTime = fpsClock.restart(); // sf::Time for frametime or FPS
+	fps[0] = fpsTime.asSeconds(); // The frametime
+	fps[1] = 1.f / fpsTime.asSeconds(); // The FPS
 }
 
+// Do both, not recommended.
 void renderer::render_scene() {
 	begin_scene();
 	end_scene();
 }
 
+// Close the window, need to revise
 void renderer::close_window() {
-	window.close();
+	window.close(); // Close the RenderWindow
 }
 
+// Set the window's state
 void renderer::setWindowState(windowState state) {
 	this->state = state;
 }
 
+// Get the FPS or frametime
 float renderer::getFPS(fpsType type) {
-	float tempfps;
-	if(type == RAW)
-		tempfps = fps[RAWFP];
-	else if(type == PERSECOND)
-		tempfps = fps[PERSC];
-	return tempfps;
+	float tempfps; // The returned time
+	if(type == RAW) // If the parameter is frametime
+		tempfps = fps[RAWFP]; // Set the returned time to frametime
+	else if(type == PERSECOND) // Else if the parameter is FPS
+		tempfps = fps[PERSC]; // Set the returned time to FPS
+	return tempfps; // Return the requested time
 }
 
-camera& renderer::getWindowCam() {
-	return windowCam;
+// Get the camera used by the window
+camera &renderer::getWindowCam() {
+	return windowCam; // Return the camera of the window
 }
--- src/pro_renderer.hpp
+++ src/pro_renderer.hpp
@@ -2,35 +2,35 @@
 #include <string>
 #include <SFML/Graphics.hpp>
 #include "pro_camera.hpp"
-#define RAWFP 0
-#define PERSC 1
+#define RAWFP 0 // Frametime
+#define PERSC 1 // FPS
 
 namespace pro {
 
+// Renderer class
 class renderer
 {
 public:
-	renderer(void);
-	enum windowState {OPEN, CLOSED};
-	enum fpsType {RAW, PERSECOND};
-	windowState state;
-	void start(bool fullscreen = false, float width = 640, float height = 480, const std::string title = "The NEW ProENGINE");
-	void begin_scene();
-	void end_scene();
-	void render_scene();
-	void close_window();
-	void setWindowState(windowState state);
-	float getFPS(fpsType type);
-	sf::RenderWindow getRW();
-	sf::RenderWindow window;
-	pro::camera& getWindowCam();
-	~renderer(void);
+	renderer(void); // Default initializer
+	enum windowState {OPEN, CLOSED}; // The state of the window
+	enum fpsType {RAW, PERSECOND}; // The type of time returned by the getFPS(fpsType type) function
+	windowState state; // The window state
+	void start(bool fullscreen = false, float width = 640, float height = 480, const std::string title = "ProENGINE 1.5.1A"); // Start the renderer with the given params
+	void begin_scene(); // Begin drawing the things
+	void end_scene(); // Stop drawing the things and display them
+	void render_scene(); // Do begin_scene() and end_scene, not recommended.
+	void close_window(); // Close the window
+	void setWindowState(windowState state); // Change the window state
+	float getFPS(fpsType type); // Get the frametime or FPS
+	sf::RenderWindow &getRW(); // Get the RenderWindow of the renderer
+	sf::RenderWindow window; // The RenderWindow of the renderer
+	pro::camera& getWindowCam(); // Get the camera of the window
+	~renderer(void); // Destructor
 private:
-	pro::camera windowCam;
-	sf::RectangleShape shape;
-	sf::Clock fpsClock;
-	sf::Time fpsTime;
-	float fps[2];
+	pro::camera windowCam; // The window's camera and viewport
+	sf::Clock fpsClock; // The sf::Clock used for gathering frametime and FPS
+	sf::Time fpsTime; // The sf::Time for converting the sf::Clock time into a float
+	float fps[2]; // The two times, frametime and FPS
 };
 
 }
