41c28736641a9819433f34d6f0af9117eab7ce06
altanh <altan.h@gmail.com>
2013-07-03 00:01:15 +0800

    Not sure...

--- projects/xcode/PROENGINE[1.5]/PROENGINE[1.5]/main.cpp
+++ projects/xcode/PROENGINE[1.5]/PROENGINE[1.5]/main.cpp
@@ -6,13 +6,24 @@
 //  Copyright (c) 2013 Dual-Pro Studios. All rights reserved.
 //
 
-#include <iostream>
+#include <SFML/Window.hpp>
 
-int main(int argc, const char * argv[])
+int main()
 {
-
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    sf::Window window(sf::VideoMode(800, 600), "My window");
+    
+    // run the program as long as the window is open
+    while (window.isOpen())
+    {
+        // check all the window's events that were triggered since the last iteration of the loop
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // "close requested" event: we close the window
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+    }
+    
     return 0;
 }
-
